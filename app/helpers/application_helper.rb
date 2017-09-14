2301f7e87ff1b744ed56c2e6932b52eecb6aa330
lukefitz1 <lukefitz1@gmail.com>
2017-09-14 18:18:10 -0400

    text encoding

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
 	def asset_data_base64(path)
-	  asset = Rails.application.assets.find_asset(path)
+	  asset = Rails.application.assets_manifest(path)
+	  # asset = CompassRails.sprockets.find_asset(path).to_s
 	  throw "Could not find asset '#{path}'" if asset.nil?
 	  base64 = Base64.encode64(asset.to_s).gsub(/\s+/, "")
 	  "data:#{asset.content_type};base64,#{Rack::Utils.escape(base64)}"
