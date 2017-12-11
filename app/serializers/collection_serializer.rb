dfb6c506940ddcab00c99945fe69a40267cafe5c
lukefitz1 <lukefitz1@gmail.com>
2017-12-11 08:40:35 -0500

    more collections api changes

--- app/serializers/collection_serializer.rb
+++ app/serializers/collection_serializer.rb
@@ -1,3 +1,3 @@
 class CollectionSerializer < ActiveModel::Serializer
-  attributes :id, :collectionName, :artworks, :identifier, :created_at, :updated_at
+  attributes :id, :collectionName, :artworks, :identifier, :customer_id, :created_at, :updated_at
 end
