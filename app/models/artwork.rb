48da3112292e58af17d68717976f47cff479fa9a
lukefitz1 <lukefitz1@gmail.com>
2019-02-07 19:55:33 -0700

    fixing the import from customers page so that the art is associated with the appropriate customer & collection

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -15,12 +15,15 @@ class Artwork < ApplicationRecord
   mount_uploader :additionalPdf, PdfUploader
 
   def self.import(file, customer_id, collection_id)
-  	# loops through csv data
+  	puts "This is the customer ID FUCK: #{customer_id}"
+    puts "This is the collection ID FUCK: #{collection_id}"
+
+    # loops through csv data
   	CSV.foreach(file.path, headers:true) do |row|
   		# create new artwork
       hash = row.to_hash
-      hash[:customer_id] = customer_id.to_i
-      hash[:collection_id] = collection_id.to_i
+      hash[:customer_id] = customer_id
+      hash[:collection_id] = collection_id
 
   		Artwork.create! hash
   	end
