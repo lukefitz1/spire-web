53fa92f6751e2663a75035390364c20416f775bd
lukefitz1 <lukefitz1@gmail.com>
2019-02-13 18:15:45 -0700

    pdf stuff

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -35,26 +35,25 @@ class CollectionsController < ApplicationController
   def pdf_crowd_table
     @collection = Collection.find(params[:coll_id])
 
-    puts "Collection: #{params[:coll_id]}"
     cookie_name = '_art_collector_web_session'
     cookie_value = cookies[:_art_collector_web_session]
-    puts "#{cookie_name}=#{cookie_value}"
-
-    # http://localhost:3000/users/sign_in
+    
+    # puts "Collection: #{params[:coll_id]}"
+    # puts "#{cookie_name}=#{cookie_value}"
 
     begin
         # create the API client instance
         client = Pdfcrowd::HtmlToPdfClient.new("spireart", "4ca5bdb67c50b7a3ca5d9a207de070e0")
         # client.setHttpAuth("lukefitz1@gmail.com", "pass4luke")
         client.setCookies("_art_collector_web_session=VEh1OEVtRFMxby9nd2dxeDEwWXZLeVhwMFlrOHMzNlAyRkpBM0tYb3FyUXBmRlgwZ3U5aGJpRkNPY0draUEvKzZhMFBUOU5aYWdpNlBBUkttbGgvdTRjcXpXY3ZrL29ETkk1V011ai9IVHdHYkV2WVBXWXZNaEcyOFRWYVNkZjVYMVhPc2Nsc3lSNDNnaXhXU3g4dGNFZEN5UE1iSnhPQUJOQ3d4aURCZ2lLVW5qZFVXdkNRcStTYTNIeW9tcFh2TkdaVzRreE8yaDV2Ynk1TjlwSDdnU1JVYUU2dW5keWduVHJQTWxIY090VT0tLTZ3YUNnQi9pWWMyenU1YXJ6YVd3NWc9PQ%3D%3D--17d5cfcaf0d9484bea841c6bfdd2789a770fd5e2")
-
-        # run the conversion and write the result to a file
-        # client.convertUrlToFile("https://spire-art-services.herokuapp.com/collections/pdf_crowd_table/7cb49999-433c-4490-85bc-0e59950f5547?coll_id=b1b51182-33b5-4f31-a546-5073678fe779", 'example.pdf')
+        # client.setCookies("#{cookie_name}=#{cookie_value}")
+        
         # create output file for conversion result
         output_file = open("example.pdf", "wb")
 
         # run the conversion and store the result into a pdf variable
-        pdf = client.convertUrl("https://spire-art-services.herokuapp.com/collections/pdf_crowd_table/7cb49999-433c-4490-85bc-0e59950f5547?coll_id=b1b51182-33b5-4f31-a546-5073678fe779")
+        # pdf = client.convertUrl("https://spire-art-services.herokuapp.com/collections/pdf_crowd_table/7cb49999-433c-4490-85bc-0e59950f5547?coll_id=b1b51182-33b5-4f31-a546-5073678fe779")
+        pdf = client.convertUrl("http://example.com/")
 
         # write the pdf into the output file
         output_file.write(pdf)
