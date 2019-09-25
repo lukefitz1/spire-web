a65f1e346086752effd2be036dd5decf79d97756
lukefitz1 <lukefitz1@gmail.com>
2019-09-24 19:49:02 -0600

    adding in type attribute to User table, adding in AdminUser class

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_02_231532) do
+ActiveRecord::Schema.define(version: 2019_09_25_013219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -128,6 +128,7 @@ ActiveRecord::Schema.define(version: 2019_09_02_231532) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "auth_token"
+    t.string "type"
     t.index ["auth_token"], name: "index_users_on_auth_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
