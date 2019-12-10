c68e60c7ba9d3749a283656f3bbccb38e05e848c
Luke Fitzgerald <lukefitzgerald@nutrien.com>
2019-12-09 18:05:27 -0700

    fixing a bug on the show view and some formatting fixes in the artwork api controller

--- app/controllers/api/artwork_controller.rb
+++ app/controllers/api/artwork_controller.rb
@@ -1,66 +1,64 @@
-class Api::ArtworkController < Api::BaseController	
-	before_action :require_login!
+class Api::ArtworkController < Api::BaseController  
+  before_action :require_login!
 
-	def index
-	@artwork = Artwork.all
-		respond_to do |format|
-			format.json { render :json => @artwork }
-		end
-	end
+  def index
+    @artwork = Artwork.all
+    respond_to do |format|
+      format.json { render :json => @artwork }
+    end
+  end
 
-	# GET /artworks/1
-	# GET /artworks/1.json
-	def show
-		@artwork = Artwork.find(params[:id])
+   # GET /artworks/1
+   # GET /artworks/1.json
+  def show
+    @artwork = Artwork.find(params[:id])
 
-		respond_to do |format|
-			format.json { render :json => @artwork }
-		end
-	end
+    respond_to do |format|
+      format.json { render :json => @artwork }
+    end
+  end
 
-	# GET /artworks/new
-	def new
-		@artwork = Artwork.new
-	end
+   # GET /artworks/new
+  def new
+    @artwork = Artwork.new
+  end
 
-	# GET /artworks/1/edit
-	def edit
-	end
+   # GET /artworks/1/edit
+  def edit
+  end
 
-	# POST /artworks
-	# POST /artworks.json
-	def create
-		@artwork = Artwork.new(artwork_params)
-		cust_id = artwork_params[:customer_id]
-		redirect = params[:redirect]
+   # POST /artworks
+   # POST /artworks.json
+  def create
+    @artwork = Artwork.new(artwork_params)
 
-		respond_to do |format|
-			if @artwork.save 
-		  		format.json { render :json => @artwork, status: :created }
-		  	else
-		  		format.json { render json: @artwork.errors, status: :unprocessable_entity }
-		  	end
-		end
-	end
+    respond_to do |format|
+      if @artwork.save 
+        format.json { render :json => @artwork, status: :created }
+      else
+        format.json { render json: @artwork.errors, status: :unprocessable_entity }
+			end
+    end
+  end
 
-	# PATCH/PUT /api/artworks/1
-  # PATCH/PUT /api/artworks/1.json
-	def update
-		@artwork = Artwork.find(params[:id])
+   # PATCH/PUT /api/artworks/1
+   # PATCH/PUT /api/artworks/1.json
+  def update
+    @artwork = Artwork.find(params[:id])
 
     respond_to do |format|
       if @artwork.update(artwork_params)
         format.json { render :json => @artwork, status: :ok }
       else
-				format.json { render json: @artwork.errors, status: :unprocessable_entity }
+        format.json { render json: @artwork.errors, status: :unprocessable_entity }
       end
     end
-	end
-	
-	# DELETE /api/artworks/1
-  # DELETE /api/artworks/1.json
-	def destroy
-		@artwork = Artwork.find(params[:id])
+  end
+  
+   # DELETE /api/artworks/1
+   # DELETE /api/artworks/1.json
+  def destroy
+    @artwork = Artwork.find(params[:id])
     @artwork.destroy
 
     respond_to do |format|
@@ -68,9 +66,9 @@ class Api::ArtworkController < Api::BaseController
     end
   end
 
-	# Never trust parameters from the scary internet, only allow the white list through.
-	def artwork_params
-		params.require(:artwork).permit(:id, :ojbId, :artType, :title, :date, :medium, :image, :description, :dimensions, :frame_dimensions, :condition, :currentLocation, :source, :dateAcquired, :amountPaid, :currentValue, :notes, :notesImage, :notesImageTwo, :additionalInfoLabel, :additionalInfoText, :additionalInfoImage, :additionalInfoImageTwo, :additionalPdf, :reviewedBy, :reviewedDate, :provenance, :artist_id, :customer_id, :remove_image, :remove_additionalInfoImage, :remove_additionalInfoImageTwo, :remove_notesImage, :remove_notesImageTwo, :collection_id, :dateAcquiredLabel, :remove_additionalPdf, :general_information_id, :show_general_info, :custom_title)
-	end
+   # Never trust parameters from the scary internet, only allow the white list through.
+  def artwork_params
+    params.require(:artwork).permit(:id, :ojbId, :artType, :title, :date, :medium, :image, :description, :dimensions, :frame_dimensions, :condition, :currentLocation, :source, :dateAcquired, :amountPaid, :currentValue, :notes, :notesImage, :notesImageTwo, :additionalInfoLabel, :additionalInfoText, :additionalInfoImage, :additionalInfoImageTwo, :additionalPdf, :reviewedBy, :reviewedDate, :provenance, :artist_id, :customer_id, :remove_image, :remove_additionalInfoImage, :remove_additionalInfoImageTwo, :remove_notesImage, :remove_notesImageTwo, :collection_id, :dateAcquiredLabel, :remove_additionalPdf, :general_information_id, :show_general_info, :custom_title)
+  end
 
 end
