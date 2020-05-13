f8aa29d045e371736f81f8d0fc35e2ccece48de2
Luke Fitzgerald <lukefitzgerald@nutrien.com>
2020-05-12 20:04:53 -0600

    removing artwork from customer serializer

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,27 +1,27 @@
 class Api::SessionsController < Api::BaseController
-	skip_before_action :require_login!, only: [:create]
+  # skip_before_action :require_login!, only: [:create]
 
-	def create
-		resource = User.find_for_database_authentication(:email => params[:user_login][:email])
-		resource ||= User.new
+  def create
+    resource = User.find_for_database_authentication(:email => params[:user_login][:email])
+    resource ||= User.new
 
-		if resource.valid_password?(params[:user_login][:password])
-		  auth_token = resource.generate_auth_token
-		  render json: { auth_token: auth_token }
-		else
-		  invalid_login_attempt
-		end
+    if resource.valid_password?(params[:user_login][:password])
+      auth_token = resource.generate_auth_token
+      render json: { auth_token: auth_token }
+    else
+      invalid_login_attempt
+    end
 
-	end
+  end
 
-	def destroy
-		resource = current_person
-		resource.invalidate_auth_token
-		head :ok
-	end
+  def destroy
+    resource = current_person
+    resource.invalidate_auth_token
+    head :ok
+  end
 
-	private
-		def invalid_login_attempt
-			render json: { errors: [ { detail:"Error with your login or password" }]}, status: 401
-		end
+  private
+  def invalid_login_attempt
+    render json: { errors: [ { detail:"Error with your login or password" }]}, status: 401
+  end
 end
--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -2,5 +2,4 @@ class CustomerSerializer < ActiveModel::Serializer
   attributes :id, :created_at, :updated_at, :firstName, :lastName, :email_address, :phone_number, :street_address, :city, :state, :zip, :referred_by, :project_notes
 
   has_many :collections
-  has_many :artworks
 end
