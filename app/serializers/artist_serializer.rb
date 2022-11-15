9225784a1d29a729f39d4800a1901c50ccdd56cf
Luke Fitzgerald <luke.fitzgerald@nutrien.com>
2022-11-14 22:01:15 -0500

    updating ruby & rails

--- app/serializers/artist_serializer.rb
+++ app/serializers/artist_serializer.rb
@@ -1,3 +1,3 @@
 class ArtistSerializer < ActiveModel::Serializer
-  attributes :id, :firstName, :lastName, :biography, :additionalInfo, :created_at, :updated_at, :artist_image
+  attributes :id, :firstName, :lastName, :biography, :additionalInfo, :created_at, :updated_at, :artist_image, :remove_artist_image
 end
