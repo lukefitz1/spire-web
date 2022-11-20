d2652f0dd13bf68e3f4000503a355dbd7fa19520
Luke Fitzgerald <luke.fitzgerald@nutrien.com>
2022-11-20 12:39:24 -0500

    removing some old rspec tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require "rspec/rails"
 require "capybara/rails"
 
-include Warden::Test::Helpers
+# include Warden::Test::Helpers
 # Warden.test_mode!
 
 begin
@@ -28,5 +28,5 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   config.include FactoryBot::Syntax::Methods
-  config.include Warden::Test::Helpers
+  # config.include Warden::Test::Helpers
 end
